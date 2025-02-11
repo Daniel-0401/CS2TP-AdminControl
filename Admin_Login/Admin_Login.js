498bdf093f45681fba1b3573adba7cd2c1942304
Daniel-0401 <240037035@aston.ac.uk>
2025-02-11 04:50:53 +0000

    Update div into page in admin login

--- Admin_Login/Admin_Login.js
+++ Admin_Login/Admin_Login.js
@@ -1,26 +1,10 @@
 const password = document.getElementById("pw"); /* showPW() */
-const divLogin = document.getElementById("login"); /* Login Section */
-const divForgot = document.getElementById("forgot"); /* Forgot Section */
-const divVerify = document.getElementById("verify"); /* Verify Section */
-const divReset = document.getElementById("reset"); /* Reset Section */
-const forgotPassword = document.getElementById("forgot_pw"); /* Forgot Password */
 
 /**  Default: type = password 
  *  Function: Show/Hidden the Password */
 function showPW() {
     password.type = (password.type === "password") ? "text" : "password";
 }
-
-/** Function: Pop Up the Corrent Section */
-function showSection(from, to) {
-    from.style.display = "none";
-    to.style.display = "block";
-}
-
-/** Default: login = block */
-forgot_pw.addEventListener("click", function(){
-    showSection(divLogin, divForgot);
-});
 
 function loginCheck() {
     
